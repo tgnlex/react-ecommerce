b3502cac05ade76c2dec1f18f619e2f4e6f34e32
tgn <alexanderwebber76@gmail.com>
2023-08-08 09:28:54 -0700

    react-ecommerce

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"; 
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+
+export const Navbar = () => {
+    return (
+        <div className="navbar">
+          <div className="links"> 
+            <Link to="/"> </Link>
+            <Link to="/cart">
+              <ShoppingCart size={32} />
+            </Link>
+        </div>
+      </div>
+          )
+}
---
+++ src/products.js
@@ -0,0 +1,58 @@
+import product1 from "./assets/1.png"
+import product2 from "./assets/2.png"
+import product3 from "./assets/3.png"
+import product4 from "./assets/4.jpg"
+import product5 from "./assets/5.jpg"
+import product6 from "./assets/6.webp"
+import product7 from "./assets/7.webp"
+import product8 from "./assets/8.webp"
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Iphone",
+        productImage: product1,
+        price: 999.0,
+    },
+    {
+        id: 2,
+        productName: "MacBook Pro",
+        productImage: product2 ,
+        price: 1899.99,
+    },
+    {
+        id: 3,
+        productName: "Cannon M50 Camera",
+        productImage:product3 ,
+        price: 699.0,
+    },
+    {
+        id: 4,
+        productName: "Van Gogh Denim Jacket",
+        productImage: product4,
+        price: 228.0,
+    },
+    {
+        id: 5,
+        productName: "LED Light Strips",
+        productImage:product5 ,
+        price: 19.99,
+    },
+    {
+        id: 6,
+        productName: "SPECTRUM LS TEE",
+        productImage:product6 ,
+        price: 68.0,
+    },
+    {
+        id: 7,
+        productName: "GOLF SHIRT",
+        productImage:product7 ,
+        price: 59.99,
+    },
+    {
+        id: 8,
+        productName: "DON'T TRIP HAT",
+        productImage:product8 ,
+        price: 17.99,
+    },
+    ]
